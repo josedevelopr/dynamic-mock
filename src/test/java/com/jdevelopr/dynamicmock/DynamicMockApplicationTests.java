58a38f033aab451d0c8c8492e2b579f74b0592e2
Jose A <jose_antonio_alvino21@outlook.es>
2023-03-07 12:32:31 -0500

    initial commit

---
+++ src/test/java/com/jdevelopr/dynamicmock/DynamicMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdevelopr.dynamicmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
