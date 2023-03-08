50e976cea6b1f015805e0e9de12707aadf849c91
Jose A <jose_antonio_alvino21@outlook.es>
2023-03-07 21:25:51 -0500

    feat: get response from mocked endpoint

--- src/main/java/com/jdevelopr/dynamicmock/web/model/ApplicationResponse.java
+++ src/main/java/com/jdevelopr/dynamicmock/web/model/ApplicationResponse.java
@@ -3,15 +3,17 @@ package com.jdevelopr.dynamicmock.web.model;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.ToString;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
 @JsonSerialize
-@ToString
 @Getter
 public class ApplicationResponse implements Serializable {
   private String code;
   private String message;
+
+  @Override public String toString() {
+    return "{\"code\":\"" + code + "\", \"message\":\"" + message + "\"}";
+  }
 }
